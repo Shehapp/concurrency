91605885da7035cf8fc40171e4298032dcedd79b
Shehapp <88383557+Shehapp@users.noreply.github.com>
2024-01-14 02:53:35 +0200

    yarab

--- Merge-Sort/MultiThreadSort/MergeSort.cs
+++ Merge-Sort/MultiThreadSort/MergeSort.cs
@@ -130,7 +130,7 @@ namespace MultiThreadSort
         
         //TODO: Define any required semaphore here
         static Semaphore []sortSemaphres;
-        static Semaphore doneSemaphore=new Semaphore(0);
+        static Semaphore[] mergeSemaphres;
         
         #endregion
 
@@ -142,6 +142,31 @@ namespace MultiThreadSort
         
         #endregion
 
+        //helper to solve it M sort and M-1 merge   M = 2^k
+        public static void breakItDown(int[] array, int curNode, int curDepth, int depthLimit, int start, int end)
+        {
+            Console.Out.Write(curNode);
+
+            if (curDepth != depthLimit)
+            {
+                mergeThreads[curNode] = new Thread(MergeMT);
+                mergeSemaphres[curNode] = new Semaphore(0);
+                mergeThreads[curNode].Start(Params2Object(array, start, end, (start + end) >> 1, curNode));
+            }
+            else if (curDepth == depthLimit)
+            {
+                int sortNode = curNode - (int)Math.Pow(2, depthLimit - 1);
+                sortThreads[sortNode] = new Thread(MSortMT);
+                sortSemaphres[sortNode] = new Semaphore(0);
+                // sortThreads[sortNode].Start(Params2Object(array, start, end, (start + end) >> 1, sortNode));
+            }
+
+            if (depthLimit == curDepth) return;
+            breakItDown(array, curNode * 2 + 1, curDepth + 1, depthLimit, start, (start + end) / 2);
+            breakItDown(array, curNode * 2 + 2, curDepth + 1, depthLimit, (start + end) / 2 + 1, end);
+        }
+
+
         #region Sort Function
 
         public static void SortMT(int[] array)
@@ -152,7 +177,20 @@ namespace MultiThreadSort
             int node_idx = 0;
 
             NumMergeSortThreads = 2; //TASK 2
-            //NumMergeSortThreads = 4;              //TASK 3
+            // NumMergeSortThreads = 4;              //TASK 3
+
+            sortThreads = new Thread[NumMergeSortThreads];
+            sortSemaphres = new Semaphore[NumMergeSortThreads];
+
+            mergeThreads = new Thread[NumMergeSortThreads-1];
+            mergeSemaphres = new Semaphore[NumMergeSortThreads-1];
+
+            // log2 x
+            int MNumMergeSortThreads = (int)Math.Ceiling(Math.Log(NumMergeSortThreads, 2));
+            breakItDown(array, 0, 0, MNumMergeSortThreads, s, e );
+            mergeSemaphres[0].Wait();
+            
+            return;
 
             #region [TASK 2]
 
@@ -164,24 +202,30 @@ namespace MultiThreadSort
                  * 3) Use semaphores to handle any dependency or critical section
                  */
 
+                
                 sortSemaphres = new Semaphore[2];
                 sortThreads = new Thread[2];
                 int ss = 1, ee = e / 2;
                 for (int i = 0; i < 2; i++)
                 {
                     sortSemaphres[i] = new Semaphore(0);
-                    
-                    sortThreads[i]=new Thread(MSortMT);
-                    sortThreads[i].Start(Params2Object(array, ss, ee, i, node_idx));
+
+                    sortThreads[i] = new Thread(MSortMT);
+                    if (i == 1) sortThreads[i].Start(Params2Object(array, ss, e, i, node_idx));
+                    else sortThreads[i].Start(Params2Object(array, ss, ee, i, node_idx));
                     ss += e / 2;
                     ee += e / 2;
                 }
-                
+
+                mergeSemaphres = new Semaphore[1];
                 mergeThreads = new Thread[1];
-                mergeThreads[0]=new Thread(MergeMT);
-                mergeThreads[0].Start(Params2Object(array, s, e, m, node_idx));
+
+                //root
+                mergeSemaphres[0] = new Semaphore(0);
+                mergeThreads[0] = new Thread(MergeMT);
+                mergeThreads[0].Start(Params2Object(array, s, e, 0, node_idx));
+                mergeSemaphres[0].Wait();
                 
-                doneSemaphore.Wait();
             }
 
             #endregion
@@ -192,9 +236,43 @@ namespace MultiThreadSort
             {
                 /*TODO: 
                  * 1) Initialize any required semaphore
-                 * 2) Create & Start TWO Merge Sort concurrent threads & ONE Merge thread
+                 * 2) Create & Start FOUR Merge Sort concurrent threads & THREE Merge thread
                  * 3) Use semaphores to handle any dependency or critical section
                  */
+                sortSemaphres = new Semaphore[4];
+                sortThreads = new Thread[4];
+                int ss = 1, ee = e / 4;
+                for (int i = 0; i < 4; i++)
+                {
+                    sortSemaphres[i] = new Semaphore(0);
+
+                    sortThreads[i] = new Thread(MSortMT);
+                    if (i == 3) sortThreads[i].Start(Params2Object(array, ss, e, i, node_idx));
+                    else sortThreads[i].Start(Params2Object(array, ss, ee, i, node_idx));
+
+                    ss += e / 4;
+                    ee += e / 4;
+                }
+
+                mergeSemaphres = new Semaphore[3];
+                mergeThreads = new Thread[3];
+
+                ss = 1;
+                ee = e / 2;
+                for (int i = 1; i < 3; i++)
+                {
+                    mergeSemaphres[i] = new Semaphore(0);
+                    mergeThreads[i] = new Thread(MergeMT);
+                    mergeThreads[i].Start(Params2Object(array, ss, ee, i, node_idx));
+                    ss += e / 2;
+                    ee += e / 2;
+                }
+
+                //root
+                mergeSemaphres[0] = new Semaphore(0);
+                mergeThreads[0] = new Thread(MergeMT);
+                mergeThreads[0].Start(Params2Object(array, s, e, 0, node_idx));
+                mergeSemaphres[0].Wait();
             }
 
             #endregion
@@ -213,16 +291,17 @@ namespace MultiThreadSort
             Object2Params(parameters, ref A, ref s, ref e, ref m, ref node_idx);
 
             #endregion
-
-
+            
             MSort(A, s, e);
-
+            sortSemaphres[node_idx].Signal(); // id of cur thread
+            
+            return;
             #region [TASK 2]
 
             if (NumMergeSortThreads == 2) //TASK 2
             {
                 //TODO: Use semaphores to handle any dependency or critical section
-                sortSemaphres[m].Signal(); // id of cur thread
+                sortSemaphres[node_idx].Signal(); // id of cur thread
             }
 
             #endregion
@@ -232,6 +311,7 @@ namespace MultiThreadSort
             else if (NumMergeSortThreads == 4) //TASK 3
             {
                 //TODO: Use semaphores to handle any dependency or critical section
+                sortSemaphres[m].Signal(); // id of cur thread
             }
 
             #endregion
@@ -249,7 +329,26 @@ namespace MultiThreadSort
             Object2Params(parameters, ref A, ref s, ref e, ref m, ref node_idx);
 
             #endregion
+            
+            int y = node_idx * 2 + 1;
+            if (y > NumMergeSortThreads - 2)
+            {
+                
+                int poo = (int)Math.Pow(2, (int)Math.Ceiling(Math.Log(NumMergeSortThreads, 2)));
+                sortSemaphres[node_idx * 2 + 2 - poo].Wait();
+                sortSemaphres[node_idx * 2 + 1 - poo].Wait();
+                Merge(A, s, m, e);
+                mergeSemaphres[node_idx].Signal();
+            }
+            else
+            {
+                mergeSemaphres[node_idx * 2 + 1].Wait();
+                mergeSemaphres[node_idx * 2 + 2 ].Wait();
+                Merge(A, s, m, e);
+                mergeSemaphres[node_idx].Signal();
+            }
 
+            return;
             #region [TASK 2]
 
             if (NumMergeSortThreads == 2) //TASK 2
@@ -257,8 +356,8 @@ namespace MultiThreadSort
                 //TODO: Use semaphores to handle any dependency or critical section
                 sortSemaphres[0].Wait();
                 sortSemaphres[1].Wait();
-                Merge(A, s, m, e);
-                doneSemaphore.Signal();
+                Merge(A, s, (s+e)/2, e);
+                mergeSemaphres[m].Signal();
             }
 
             #endregion
@@ -268,7 +367,22 @@ namespace MultiThreadSort
             else if (NumMergeSortThreads == 4) //TASK 3
             {
                 //TODO: Use semaphores to handle any dependency or critical section
-                Merge(A, s, m, e);
+                if (m == 0)
+                {
+                    mergeSemaphres[1].Wait();
+                    mergeSemaphres[2].Wait();
+                    Merge(A, s, (s + e) / 2, e);
+                    mergeSemaphres[m].Signal();
+                }
+                else
+                {
+                    sortSemaphres[m*2-2].Wait();
+                    sortSemaphres[m*2-1].Wait();
+                    Merge(A, s, (s + e) / 2, e);
+                    mergeSemaphres[m].Signal();
+                }
+
+
             }
 
             #endregion
